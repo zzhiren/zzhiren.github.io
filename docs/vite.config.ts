ddf9fe62e230962caf284956a960c14c6d70ce62
zzhiren <Zzhiren@520.com>
2023-11-16 16:09:05 +0800

    chore: 更新deploy.yml

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -7,24 +7,24 @@ const fs = _fs.promises
 
 export default defineConfig({
   clearScreen: false,
-  plugins: [
-    ...(process.env.NETLIFY ? [] : [copyPiniaPlugin()]),
-    // TODO: actual plugin that works well
-    // TypeDocPlugin({
-    //   name: 'Pinia',
-    //   entryPoints: [
-    //     path.resolve(__dirname, '../pinia/src/index.ts'),
-    //     path.resolve(__dirname, '../testing/src/index.ts'),
-    //     path.resolve(__dirname, '../nuxt/src/index.ts'),
-    //   ],
-    // }),
-  ],
+  // plugins: [
+  //   ...(process.env.NETLIFY ? [] : [copyPiniaPlugin()]),
+  //   // TODO: actual plugin that works well
+  //   // TypeDocPlugin({
+  //   //   name: 'Pinia',
+  //   //   entryPoints: [
+  //   //     path.resolve(__dirname, '../pinia/src/index.ts'),
+  //   //     path.resolve(__dirname, '../testing/src/index.ts'),
+  //   //     path.resolve(__dirname, '../nuxt/src/index.ts'),
+  //   //   ],
+  //   // }),
+  // ],
   define: {
     __DEV__: 'true',
     __BROWSER__: 'true',
   },
   optimizeDeps: {
-    exclude: ['vue-demi', '@vueuse/shared', '@vueuse/core', 'pinia'],
+    exclude: ['vue-demi', '@vueuse/shared', '@vueuse/core'],
   },
 })
 
